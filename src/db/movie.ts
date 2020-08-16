b34849aa39f61ad53f1543f164e22b024e0856b9
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2020-08-16 18:47:11 -0300

    refactor error message

--- src/db/movie.ts
+++ src/db/movie.ts
@@ -4,7 +4,7 @@ import { QueryResult, PoolClient } from 'pg';
 export const fetchMovies = async (): Promise<any> => {
     const client: PoolClient = await pool.connect();
     const movies: QueryResult | any = await client.query('SELECT * FROM movie', [])
-        .catch(e => { return { error: true, message: "Error when try to get movies", data: [] } })
+        .catch(e => { return { error: true, message: `${e}`, data: [] } })
         .finally(() => client.release());
     return movies.rows !== undefined ?
         { error: false, message: "Ok", data: movies.rows } :
